6286ac65676d8800ce988279acaae5ace9aca23f
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2024-04-27 15:49:00 +0200

    #62 Modell för patienter

--- Hjalpmedelskollen/Migrations/20240427134652_PatientModel.Designer.cs
+++ Hjalpmedelskollen/Migrations/20240427134652_PatientModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Hjalpmedelskollen.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Hjalpmedelskollen.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240427134652_PatientModel")]
+    partial class PatientModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -42,9 +45,6 @@ namespace Hjalpmedelskollen.Migrations
                     b.Property<string>("Location")
                         .HasMaxLength(50)
                         .HasColumnType("character varying(50)");
-
-                    b.Property<int?>("PatientId")
-                        .HasColumnType("integer");
 
                     b.Property<string>("ProductName")
                         .IsRequired()
---
+++ Hjalpmedelskollen/Migrations/20240427135122_PatientModelDbSet.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hjalpmedelskollen.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatientModelDbSet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PatientId",
+                table: "Aids",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientId",
+                table: "Aids");
+        }
+    }
+}
