d38b930adc269f234a5f19573e7a97c395ebc843
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2024-04-30 11:41:30 +0200

    #65 Lägga till PatientId när man lägger till hjälpmedel

--- Hjalpmedelskollen/Controllers/HomeController.cs
+++ Hjalpmedelskollen/Controllers/HomeController.cs
@@ -74,6 +74,13 @@ namespace Hjalpmedelskollen.Controllers
             {
                 try
                 {
+                    if (aid.Location != "Förråd" && aid.Location != "Avdelning")
+                    {
+                        string[] parts = aid.Location.Split(' ');
+                        int patientNumber = int.Parse(parts[0]);
+                        aid.PatientId = patientNumber;
+                    }
+
                     string inspection = Request.Form["Inspection"].ToString();
                     int? selectedMonth = null;
 
