4784da7b1bdb50ff587291ea8db7db53e81ca199
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2024-05-12 12:13:50 +0200

    #95 Påbörjat gränssnitt för sök

---
+++ Hjalpmedelskollen/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using Hjalpmedelskollen.DAL;
+using Hjalpmedelskollen.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hjalpmedelskollen.Controllers
+{
+	public class SearchController : Controller
+	{
+		private readonly IDbRepository _dbRepository;
+
+		public SearchController(IDbRepository dbRepository)
+		{
+			_dbRepository = dbRepository;
+		}
+
+		public async Task <IActionResult> Index()
+		{
+			var viewModel = await GetSearchViewModel();
+			return View(viewModel);
+			//return View();
+		}
+		public async Task<SearchViewModel> GetSearchViewModel()
+		{
+			var units = await _dbRepository.GetUnits();
+			var viewModel = new SearchViewModel()
+			{
+				Units = units
+			};
+
+			return viewModel;
+		}
+
+
+	}
+}
