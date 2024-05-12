c9d1d5251a2e9be7262fe16a935a8cbb8979b6f9
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2024-05-12 23:40:18 +0200

    Enklare gränssnitt för dokumenthantering

---
+++ Hjalpmedelskollen/Controllers/DocumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hjalpmedelskollen.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
