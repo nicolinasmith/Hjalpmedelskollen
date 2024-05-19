d96013c61b98e23b5c4879e6a093df0dc0d60cbf
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2024-05-19 14:03:47 +0200

    Uppdatera söktabell vid förändring

--- Hjalpmedelskollen/wwwroot/js/search.js
+++ Hjalpmedelskollen/wwwroot/js/search.js
@@ -63,56 +63,6 @@
             tableBody.appendChild(tr);
         });
 
-        /*AIDS BY UNIT - SORT TABLE*/
-        /*SOURCE: https://www.w3schools.com/howto/howto_js_sort_table.asp*/
-        //function sortTable(n, selectedTable) {
-        //    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-        //    table = document.getElementById(selectedTable);
-        //    switching = true;
-        //    dir = "asc";
-        //    while (switching) {
-        //        switching = false;
-        //        rows = table.rows;
-        //        for (i = 1; i < (rows.length - 1); i++) {
-        //            shouldSwitch = false;
-        //            x = rows[i].getElementsByTagName("TD")[n];
-        //            y = rows[i + 1].getElementsByTagName("TD")[n];
-        //            if (dir == "asc") {
-        //                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //                    shouldSwitch = true;
-        //                    break;
-        //                }
-        //            } else if (dir == "desc") {
-        //                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-        //                    shouldSwitch = true;
-        //                    break;
-        //                }
-        //            }
-        //        }
-        //        if (shouldSwitch) {
-        //            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        //            switching = true;
-        //            switchcount++;
-        //        } else {
-        //            if (switchcount == 0 && dir == "asc") {
-        //                dir = "desc";
-        //                switching = true;
-        //            }
-        //        }
-        //    }
-        //}
-
-        //function addSortEventToHeader(tableId) {
-        //    var headers = document.querySelectorAll('#' + tableId + ' .table-header th');
-        //    headers.forEach(function (header, index) {
-        //        header.addEventListener('click', function () {
-        //            sortTable(index, tableId);
-        //        });
-        //    });
-        //}
-
-        //addSortEventToHeader('search-table');
-
         /*UPDATE AID*/
         var aidRows = document.querySelectorAll('.aid-row');
         var aidPopup = document.getElementById('update-aid-popup');
@@ -211,8 +161,9 @@
                 },
                 success: function (response) {
                     if (response.success) {
-                        alert('Hjälpmedlet har uppdaterats. Tabellen behöver uppdateras för att ändringarna ska visas.');
+                        searchAid();
                         $('#update-aid-popup').hide();
+                        alert('Hjälpmedlet har uppdaterats.');
                     } else {
                         alert('Det gick inte att uppdatera hjälpmedlet.');
                     }
@@ -237,8 +188,7 @@
                 success: function (response) {
                     if (response.success) {
                         $('#update-aid-popup').hide();
-                        $('.aid-row[data-id="' + id + '"]').addClass('disabled-aid-row');
-                        $('.aid-row[data-id="' + id + '"]').removeClass('aid-row');
+                        searchAid();
                         alert('Hjälpmedlet har tagits bort.');
                     } else {
                         alert('Det gick inte att ta bort hjälpmedlet.');
